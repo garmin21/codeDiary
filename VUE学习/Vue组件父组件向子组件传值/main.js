e7ba83e2a559dc735e7583ba142505f1405d6451
Lj <2180727350@qq.com>
2020-02-12 21:51:47 +0800

    'xxx‘

--- VUE学习/Vue组件父组件向子组件传值/main.js
+++ VUE学习/Vue组件父组件向子组件传值/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+/* 
+render方法的作用是 ： 用来创建 html
+h : 表示 creataElement(),会默认传入一个这样的方法
+render 方法调用结束后会返回一个 完整的 DOM树,
+最后通过 vm 实例的 $mount('#app),挂载到页面上，并替换 #app 
+*/
 new Vue({
   render: h => h(App),
 }).$mount('#app')
