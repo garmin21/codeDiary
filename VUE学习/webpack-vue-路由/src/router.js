ad36a43948338069b96f99dda811bbd626fc4053
Lj <2180727350@qq.com>
2020-02-15 19:05:46 +0800

    添加了一些vue没有学过的知识点

--- VUE学习/webpack-vue-路由/src/router.js
+++ VUE学习/webpack-vue-路由/src/router.js
@@ -1,37 +1,31 @@
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 
-import login from './login.vue'
-import register  from './register.vue'
+import login from "./login.vue";
+import register from "./register.vue";
 
 // 子路由
-import son1 from './subcom/son1.vue'
-import son2 from './subcom/son2.vue'
-
-
+import son1 from "./subcom/son1.vue";
+import son2 from "./subcom/son2.vue";
 
 // 创建路由模块对象
 var router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: "/", redirect: "/login" },
 
-    routes: [
-  
-      { path: '/', redirect: '/login' },
-  
-      { 
-          path: '/login', 
-          component: login,
-          children: [
-              {  path: 'son1', component : son1 },
-              {  path: 'son2', component : son2 }
-          ]
-      },
-  
-      { path: '/register', component: register }
-  
-    ]
-  
-  });
+    {
+      path: "/login",
+      component: login,
+      children: [
+        { path: "son1", component: son1 },
+        { path: "son2", component: son2 }
+      ]
+    },
 
+    { path: "/register", component: register }
+  ]
+});
 
 // 暴露router对象
 
-export default router
+export default router;
